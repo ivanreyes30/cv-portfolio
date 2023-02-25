ea56f03f357c83900707086127af3f91f2b54e28
ivan-reyes-bizmatesph <ivan.reyes@bizmates.ph>
2023-02-25 15:41:07 +0800

    fixed development stage docker

---
+++ api/start.sh
@@ -0,0 +1,8 @@
+# uninstall the current bcrypt modules
+npm uninstall bcrypt @types/bcrypt
+
+# install the bcrypt modules for the machine
+npm install bcrypt
+
+# install the bcrypt typescript modules for the machine
+npm install --save-dev @types/bcrypt
